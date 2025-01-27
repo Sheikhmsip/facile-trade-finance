c38a6d7cf11ec85e45e18bec83e29cbb5a2a14fd
Sheikhmsip <sheikhmsip@gmail.com>
2025-01-27 16:56:14 +0600

    setup project with all necessary packages and folder structure

---
+++ facile-trade-finance-frontend/src/Pages/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='text-2xl font-bold'>
+            this is service page 
+        </div>
+    );
+};
+
+export default Services;
